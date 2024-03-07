410167996222799f1b1ac7f416c2686b993e394c
Nisha70 <135499499+Nisha70@users.noreply.github.com>
2024-03-07 20:25:06 +0530

    Add files via upload

---
+++ bank_object.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include"bank.h"
+using namespace std;
+
+int main()
+{
+	Bank b1,b2,b3;
+	b1.addbranch("we",43);
+	b2.addbranch("e",23);
+	b3.addbranch("weqq",23);
+	b1.allbranch();
+}
